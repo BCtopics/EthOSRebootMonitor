45ad2d404954b6785c8abec0d8f5f1b9fbfd097c
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2018-08-16 15:40:13 -0400

    Add, and build out SelectionScreenViewController

---
+++ EthOSRebootMonitor/EthOSRebootMonitor/ViewControllers/SelectionScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SelectionScreenViewController.swift
+//  EthOSRebootMonitor
+//
+//  Created by Bradley GIlmore on 8/16/18.
+//  Copyright © 2018 Bradley Gilmore. All rights reserved.
+//
+
+import Cocoa
+
+class SelectionScreenViewController: NSViewController {
+
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func yesButtonTapped(_ sender: Any) {
+        // Perform segue to the automatic entry version
+    }
+    
+    @IBAction func noButtonTapped(_ sender: Any) {
+        // Perform segue to the manual entry version
+    }
+}
