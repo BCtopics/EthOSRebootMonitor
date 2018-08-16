e16612bdab2190aacc9d5939452e962b4325afe5
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2018-08-16 14:30:49 -0400

    Initial Xcode Project Creation

---
+++ EthOSRebootMonitor/EthOSRebootMonitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EthOSRebootMonitor
+//
+//  Created by Bradley GIlmore on 8/16/18.
+//  Copyright © 2018 Bradley Gilmore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EthOSRebootMonitor/EthOSRebootMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EthOSRebootMonitor
+//
+//  Created by Bradley GIlmore on 8/16/18.
+//  Copyright © 2018 Bradley Gilmore. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
