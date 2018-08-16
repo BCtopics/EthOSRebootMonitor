27edff15658356ee957c10475ab09e381fa8d9f3
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2018-08-16 16:17:22 -0400

    Add EthOSPanelLetterViewController

---
+++ EthOSRebootMonitor/EthOSRebootMonitor/Models/EthOSAccount.swift
@@ -0,0 +1,13 @@
+//
+//  EthOSAccount.swift
+//  EthOSRebootMonitor
+//
+//  Created by Bradley GIlmore on 8/16/18.
+//  Copyright © 2018 Bradley Gilmore. All rights reserved.
+//
+
+import Foundation
+
+class EthOSAccount {
+    
+}
