7bc770d6726c895365a312dc83ada49a0903dfe3
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2018-08-16 14:52:21 -0400

    Add NMSSH Framework, add briding headers

---
+++ EthOSRebootMonitor/EthOSRebootMonitor/EthOSRebootMonitor-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "NMSSH/libssh2_sftp.h"
+#import "NMSSH/libssh2.h"
+#import "NMSSH/NMSFTP.h"
+#import "NMSSH/NMSFTPFile.h"
+#import "NMSSH/NMSSH.h"
+#import "NMSSH/NMSSHChannel.h"
+#import "NMSSH/NMSSHCHannelDelegate.h"
+#import "NMSSH/NMSSHConfig.h"
+#import "NMSSH/NMSSHHostConfig.h"
+#import "NMSSH/NMSSHLogger.h"
+#import "NMSSH/NMSSHSession.h"
+#import "NMSSH/NMSSHSessionDelegate.h"
